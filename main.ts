38e079005beaf755b34039c5aacd6389c8c3cad3
Danny Clyde <Dantheman27c@gmail.com>
2022-06-15 00:12:42 -0600

    cleaning up main

--- main.ts
+++ main.ts
@@ -3,25 +3,26 @@ import { PuzzleSolver, SolutionResult } from './puzzle-solver/PuzzleSolver.ts';
 import { Puzzle } from './puzzle-solver/Puzzle.ts';
 
 const puzzleFileHelper = new PuzzleFileHelper();
+const solver = new PuzzleSolver();
 
 for await (const file of Deno.readDir('SamplePuzzles/Input')) {
   let puzzle: Puzzle;
-  let res: SolutionResult = { };
+  let res: SolutionResult = {};
   console.log('solving', file.name);
   try {
     puzzle = await puzzleFileHelper.readPuzzleFromFile(`SamplePuzzles/Input/${file.name}`);
   } catch (e) {
-    res = {
-      error: e.message,
-    };
-    await puzzleFileHelper.writePuzzleSolutionToFile(`SamplePuzzles/Output/${file.name}`, res);
+    await failedToReadPuzzle(e, res, file.name);
     continue;
   }
-  if (!res.error) {
-    const solver = new PuzzleSolver();
-    solver.setPuzzle(puzzle);
-    res = solver.solve();
-    console.log(!!res.error);
-    await puzzleFileHelper.writePuzzleSolutionToFile(`SamplePuzzles/Output/${file.name}`, res);
-  }
+  solver.setPuzzle(puzzle);
+  res = solver.solve();
+  await puzzleFileHelper.writePuzzleSolutionToFile(`SamplePuzzles/Output/${file.name}`, res);
+}
+
+async function failedToReadPuzzle(e: Error, res: SolutionResult, fileName: string) {
+  res = {
+    error: e.message,
+  };
+  await puzzleFileHelper.writePuzzleSolutionToFile(`SamplePuzzles/Output/${fileName}`, res);
 }
